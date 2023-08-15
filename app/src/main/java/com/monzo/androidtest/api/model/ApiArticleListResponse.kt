905c3690e5cee8320cff558d0a971d083e2c16e2
Shayne3000 <alfred3000prime@hotmail.com>
2023-08-15 15:33:41 -0400

    refactoring

--- app/src/main/java/com/monzo/androidtest/api/model/ApiArticleListResponse.kt
+++ app/src/main/java/com/monzo/androidtest/api/model/ApiArticleListResponse.kt
@@ -1,5 +1,7 @@
 package com.monzo.androidtest.api.model
 
+import com.monzo.androidtest.data.ApiArticle
+
 data class ApiArticleListResponse(val response: ApiArticleList)
 
 data class ApiArticleList(val results: List<ApiArticle>)
---
+++ app/src/main/java/com/monzo/androidtest/data/ArticlesRepository.kt
@@ -0,0 +1,26 @@
+package com.monzo.androidtest.data
+
+import com.monzo.androidtest.data.remote.ArticleRemoteService
+import com.monzo.androidtest.di.ThreadingModule
+import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.core.Single
+import javax.inject.Inject
+import javax.inject.Named
+
+/**
+ * Responsible for coordinating paged Article data retrieval from
+ * the remote Guardian service for higher layers in the hierarchy.
+ */
+class ArticleRepository @Inject constructor(
+    private val articleApi: ArticleRemoteService,
+    @Named(ThreadingModule.IO) private val ioScheduler: Scheduler,
+    @Named(ThreadingModule.UI) private val uiScheduler: Scheduler
+) {
+    fun latestFintechArticles(): Single<List<ArticleGroupItem>> {
+        return articleApi.searchArticles().map {
+            it.response.results.toExternalGroupList()
+        }.subscribeOn(ioScheduler).observeOn(uiScheduler)
+        // TODO Consider Building the detail view.
+        // TODO Replace this whole setup with a Paging flow driven by the DB
+    }
+}
